ff19216f8c98bfe68b8270e7c77e2d0b9d363ed4
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-07-17 21:55:53 +0100

    Get user input.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("RustCalc prototype.");
+    println!("Press 'Ctrl+c' to exit.");
+    println!("Input h/H for help.");
+    println!("\n");
+
+    loop {
+        let mut expression = String::new();
+
+        print!("RustCalc> ");
+        let _ = stdout().flush();
+
+        stdin().read_line(&mut expression).expect("Incorrect expression.");
+        if let Some('\n') = expression.chars().next_back() {
+            expression.pop();
+        }
+        if let Some('\r') = expression.chars().next_back() {
+            expression.pop();
+        }
+
+        println!("Echo: {expression}");
+    }
 }
